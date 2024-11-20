0431ed8e89f8dd4caac89ca4d763f182c7da64ee
joaovitorsander <joaosandervitorsantos@gmail.com>
2024-11-20 15:09:31 -0300

    ajustado componente de data na página de times

--- front/src/pages/TeamsPage.vue
+++ front/src/pages/TeamsPage.vue
@@ -4,30 +4,82 @@
       <div class="form-section">
         <q-card class="q-pa-md custom-card">
           <q-form @submit.prevent="submitTime">
-            <q-input filled v-model="nome_time" label="Nome do Time" class="q-mb-md" />
-            <q-input filled v-model="sigla_time" label="Sigla do Time" class="q-mb-md" />
-            <q-uploader label="Imagem do Time" @added="onFileAdded" class="q-mb-md" flat bordered color="primary" />
-            <q-input filled v-model="observacao" label="Observação" type="textarea" class="q-mb-md" />
-            <q-input filled v-model="data_registro" mask="##/##/####" label="Data de Registro" class="q-mb-md">
-              <template v-slot:append>
-                <q-icon name="event" />
-              </template>
-            </q-input>
-            <q-btn type="submit" :label="editMode ? 'ATUALIZAR' : 'SALVAR'" color="primary"
-              class="full-width q-mt-lg save-button" />
-            <q-btn @click="resetForm" label="NOVO" color="secondary" class="full-width q-mt-md save-button" />
+            <q-input
+              filled
+              v-model="nome_time"
+              label="Nome do Time"
+              class="q-mb-md"
+            />
+            <q-input
+              filled
+              v-model="sigla_time"
+              label="Sigla do Time"
+              class="q-mb-md"
+            />
+            <q-uploader
+              label="Imagem do Time"
+              @added="onFileAdded"
+              class="q-mb-md"
+              flat
+              bordered
+              color="primary"
+            />
+            <q-input
+              filled
+              v-model="observacao"
+              label="Observação"
+              type="textarea"
+              class="q-mb-md"
+            />
+            <q-input
+              filled
+              v-model="data_registro"
+              type="date"
+              label="Data de Registro"
+              outlined
+              class="q-mb-md"
+            />
+            <q-btn
+              type="submit"
+              :label="editMode ? 'ATUALIZAR' : 'SALVAR'"
+              color="primary"
+              class="full-width q-mt-lg save-button"
+            />
+            <q-btn
+              @click="resetForm"
+              label="NOVO"
+              color="secondary"
+              class="full-width q-mt-md save-button"
+            />
           </q-form>
         </q-card>
       </div>
 
       <div class="list-section">
         <q-card class="q-pa-md custom-card">
-          <q-table v-if="Array.isArray(times) && times.length > 0" :rows="times" :columns="columns" row-key="id"
-            title="Times Cadastrados" dense flat bordered>
+          <q-table
+            v-if="Array.isArray(times) && times.length > 0"
+            :rows="times"
+            :columns="columns"
+            row-key="id"
+            title="Times Cadastrados"
+            dense
+            flat
+            bordered
+          >
+            <template v-slot:body-cell-data_registro="props">
+              <q-td :props="props">{{ props.row.data_registro }}</q-td>
+            </template>
             <template v-slot:body-cell-actions="props">
               <q-td :props="props" align="right">
                 <q-btn flat round icon="edit" @click="loadTime(props.row)" />
-                <q-btn flat round icon="delete" color="negative" @click="deleteTime(props.row.id)" />
+                <q-btn
+                  flat
+                  round
+                  icon="delete"
+                  color="negative"
+                  @click="deleteTime(props.row.id)"
+                />
               </q-td>
             </template>
           </q-table>
@@ -39,26 +91,50 @@
 </template>
 
 <script setup>
-import { ref, onMounted } from 'vue';
-import { useTimesStore } from 'src/stores/teamStore';
+import { ref, onMounted } from "vue";
+import { useTimesStore } from "src/stores/teamStore";
 
 const store = useTimesStore();
 
-const nome_time = ref('');
-const sigla_time = ref('');
-const observacao = ref('');
-const data_registro = ref('');
+const nome_time = ref("");
+const sigla_time = ref("");
+const observacao = ref("");
+const data_registro = ref("");
 const selectedFile = ref(null);
 const times = ref([]);
 const currentTeamId = ref(null);
 const editMode = ref(false);
 
 const columns = [
-  { name: 'nome_time', label: 'Nome do Time', align: 'left', field: 'nome_time', sortable: true },
-  { name: 'sigla_time', label: 'Sigla', align: 'left', field: 'sigla_time', sortable: true },
-  { name: 'observacao', label: 'Observação', align: 'left', field: 'observacao', sortable: true },
-  { name: 'data_registro', label: 'Data de Registro', align: 'left', field: 'data_registro', sortable: true },
-  { name: 'actions', label: 'Ações', align: 'center', field: 'actions' }
+  {
+    name: "nome_time",
+    label: "Nome do Time",
+    align: "left",
+    field: "nome_time",
+    sortable: true,
+  },
+  {
+    name: "sigla_time",
+    label: "Sigla",
+    align: "left",
+    field: "sigla_time",
+    sortable: true,
+  },
+  {
+    name: "observacao",
+    label: "Observação",
+    align: "left",
+    field: "observacao",
+    sortable: true,
+  },
+  {
+    name: "data_registro",
+    label: "Data de Registro",
+    align: "left",
+    field: "data_registro",
+    sortable: true,
+  },
+  { name: "actions", label: "Ações", align: "center", field: "actions" },
 ];
 
 onMounted(async () => {
@@ -78,7 +154,7 @@ async function submitTime() {
     sigla_time: sigla_time.value,
     observacao: observacao.value,
     data_registro: data_registro.value,
-    imagem_time: selectedFile.value ? selectedFile.value.name : ''
+    imagem_time: selectedFile.value ? selectedFile.value.name : "",
   };
 
   if (editMode.value) {
@@ -95,7 +171,7 @@ function loadTime(time) {
   nome_time.value = time.nome_time;
   sigla_time.value = time.sigla_time;
   observacao.value = time.observacao;
-  data_registro.value = time.data_registro;
+  data_registro.value = time.data_registro; // Certifique-se de que a data esteja no formato yyyy-MM-dd
   currentTeamId.value = time.id;
   editMode.value = true;
 }
@@ -106,10 +182,10 @@ async function deleteTime(id) {
 }
 
 function resetForm() {
-  nome_time.value = '';
-  sigla_time.value = '';
-  observacao.value = '';
-  data_registro.value = '';
+  nome_time.value = "";
+  sigla_time.value = "";
+  observacao.value = "";
+  data_registro.value = "";
   selectedFile.value = null;
   editMode.value = false;
   currentTeamId.value = null;
